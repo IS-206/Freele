8f76162af2aa9d67b4f16bd19785dcffba76c58d
Vetle Augustin Framnes <vetle.augustin@gmail.com>
2014-10-05 19:24:46 +0200

    GUI
    Added GUI for the client part of the application.

---
+++ project/FreeleClient/src/freeleclient/Freele.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package freeleclient;
+
+/**
+ *
+ * @author Vetle
+ */
+public class Freele {
+    public static void main(String args[]){
+        new FreeleClient().setVisible(true);
+    }
+}
