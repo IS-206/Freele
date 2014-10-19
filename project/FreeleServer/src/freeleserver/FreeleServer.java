6621664134894a6cb92a7ca8c9a27deec83dc0b4
Khomme <kjetilhomme39@gmail.com>
2014-10-19 19:57:05 +0200

    Constrctor
    Added the construtor

--- project/FreeleServer/src/freeleserver/FreeleServer.java
+++ project/FreeleServer/src/freeleserver/FreeleServer.java
@@ -4,6 +4,7 @@
 package freeleserver;
 
 import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
@@ -33,6 +34,15 @@ public class FreeleServer {
          Connection socket
          */
         public UserServer(Socket clientSocket, PrintWriter user) {
+            client = user;
+            try {
+                socket = clientSocket;
+                InputStreamReader isReader = new InputStreamReader(socket.getInputStream());
+                bufferedReader = new BufferedReader(isReader);
+            }
+            catch (Exception ex) {
+                System.out.println("Error beginning StreamReader. \n");
+            }           
         }
 //--------------------------------------------------------------------------------------------------------        
 
